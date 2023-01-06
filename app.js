ca3e5d98c12bade0bd2c0000bc6208b0ec569eb5
Rodneysia <rodneysia.musgraves@gmail.comgit config --global user.name Rodneysiagit config --global user.email rodneysia.musgraves@gmail.com>
2023-01-05 21:25:16 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(morgan('dev'));
 
 const PORT = 1337;
 
